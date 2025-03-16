5c402ecf0339edb7e69e9e28c5619bb8db708ec5
rnat200 <jmz3qqvt@tutamail.com>
2025-03-16 23:01:18 +0300

    reworked project & add solution 789

--- functions.cpp
+++ functions.cpp
@@ -1,21 +1,201 @@
 #include "header.h"
 #include <iostream>
 
-void InputTypeElements(char& typeElements)
-{
-	std::cout << "input type of elements 'i' - int, 'd' - double, 'c' - char" << '\n';
-	std::cin >> typeElements;
+void InputTypeElements(char& type) {
+    std::cout << "Enter element type ('i', 'd', 'c'): ";
+    std::cin >> type;
+}
+
+void InputSize(int32_t& size) {
+    std::cout << "Enter array size: ";
+    std::cin >> size;
+}
+
+bool CheckSize(int32_t size) {
+    return size > 0 && size < 100;
 }
 
 
-void InputSize(int32_t& size)
-{
-	std::cout << "input size of array\n";
-	std::cin >> size;
+void Task123() {
+    int32_t size{};
+    char type{};
+
+    InputTypeElements(type);
+    system("cls");
+    std::cout << "[Block 123 (Made by A.Safronova)]\n";
+    switch (type)
+    {
+    case 'i':
+        InputSize(size);
+        if (CheckSize(size))
+        {
+            int64_t intArray[MAX_SIZE];
+            int64_t intElement{};
+            std::cout << "input elements of intarray\n";
+            InputArray(intArray, size);
+            system("cls");
+            std::cout << "intarray: ";
+            PrintArray(intArray, size);
+            std::cout << '\n';
+            InputElement(intElement);
+            std::cout << "position of element: " << FindElement(intArray, size, intElement);
+            std::cout << '\n';
+            std::cout << "number of elements between zero elements: " << CountElements(intArray, size);
+            std::cout << '\n';
+        }
+        else
+        {
+            std::cout << "wrong size\n";
+        }
+        break;
+    case 'd':
+        InputSize(size);
+        if (CheckSize(size))
+        {
+            double doubleArray[MAX_SIZE];
+            double doubleElement{};
+            std::cout << "input elements of doublearray\n";
+            InputArray(doubleArray, size);
+            system("cls");
+            std::cout << "doublearray: ";
+            PrintArray(doubleArray, size);
+            std::cout << '\n';
+            InputElement(doubleElement);
+            std::cout << "position of element: " << FindElement(doubleArray, size, doubleElement);
+            std::cout << '\n';
+            std::cout << "number of elements between zero elements: " << CountElements(doubleArray, size);
+            std::cout << '\n';
+        }
+        else
+        {
+            std::cout << "wrong size\n";
+        }
+        break;
+    case 'c':
+        InputSize(size);
+        if (CheckSize(size))
+        {
+            char charArray[MAX_SIZE];
+            char charElement{};
+            std::cout << "input elements of chararray\n";
+            InputArray(charArray, size);
+            system("cls");
+            std::cout << "doublearray: ";
+            PrintArray(charArray, size);
+            std::cout << '\n';
+            InputElement(charElement);
+            std::cout << "position of element: " << FindElement(charArray, size, charElement);
+            std::cout << '\n';
+            std::cout << "number of elements between zero elements: " << CountElements(charArray, size);
+            std::cout << '\n';
+        }
+        else
+        {
+            std::cout << "wrong size\n";
+        }
+        break;
+
+    default:
+        std::cout << "wrong type of elements\n";
+        break;
+    }
 }
 
 
-bool CheckSize(int32_t size)
-{
-	return 0 < size < 100;
+void Task456() {
+   
+    int32_t size{};
+    char type{};
+
+    InputTypeElements(type);
+    InputSize(size);
+    if (!CheckSize(size)) {
+        std::cout << "Invalid size!\n";
+        return;
+    }
+
+    system("cls");
+    std::cout << "[Block 456 (Made by A.Davydov)] ";
+    switch (type) {
+    case 'i': 
+        int64_t intArray[MAX_SIZE]; InputArray(intArray, size);
+
+    break; 
+    case 'd': 
+        double doubleArray[MAX_SIZE]; InputArray(doubleArray, size);
+       
+        break; 
+    case 'c':
+        char charArray[MAX_SIZE]; InputArray(charArray, size);
+
+        break; 
+    default: std::cout << "Invalid type!\n";
+    }
+    
 }
+
+void Task789() {
+
+    int32_t size{};
+    char type{};
+
+    InputTypeElements(type);
+    InputSize(size);
+    if (!CheckSize(size)) {
+        std::cout << "Invalid size!\n";
+        return;
+    }
+
+    system("cls");
+    std::cout << "[Block 789 (Made by R.Ustinov)]\n";
+    switch (type) {
+    case 'i':  
+        int64_t intArray[MAX_SIZE];
+        int64_t TempintArray[MAX_SIZE];
+        InputArray(intArray, size);
+        PrintArray(intArray, size);
+        CopyArray(intArray, TempintArray, size);
+        BubbleSort(intArray, size);
+        PrintArray(intArray, size);
+        CopyArray(TempintArray, intArray, size);
+        ReverseArray(intArray, size);
+        PrintArray(intArray, size);
+        CopyArray(TempintArray, intArray, size);
+        RemoveNegative(intArray, size);
+        PrintArray(intArray, size);
+        break; 
+    case 'd': 
+        double doubleArray[MAX_SIZE];
+        double TempdoubleArray[MAX_SIZE];
+        InputArray(doubleArray, size);
+        PrintArray(doubleArray, size);
+        CopyArray(doubleArray, TempdoubleArray, size);
+        BubbleSort(doubleArray, size);
+        PrintArray(doubleArray, size);
+        CopyArray(TempdoubleArray, doubleArray, size);
+        ReverseArray(doubleArray, size);
+        PrintArray(doubleArray, size);
+        CopyArray(TempdoubleArray, doubleArray, size);
+        RemoveNegative(doubleArray, size);
+        PrintArray(doubleArray, size);
+        break; 
+    case 'c': 
+        char charArray[MAX_SIZE]; 
+        char TempcharArray[MAX_SIZE];
+        InputArray(charArray, size);
+        PrintArray(charArray, size);
+        CopyArray(charArray, TempcharArray, size);
+        BubbleSort(charArray, size);
+        PrintArray(charArray, size);
+        CopyArray(TempcharArray, charArray, size);
+        ReverseArray(charArray, size);
+        PrintArray(charArray, size);
+        CopyArray(TempcharArray, charArray, size);
+        RemoveNegative(charArray, size);
+        PrintArray(charArray, size);
+        break; 
+    default: std::cout << "Invalid type!\n";
+    }
+    
+}
+
